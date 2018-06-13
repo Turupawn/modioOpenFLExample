dd6dd76b404ea67d5918b34fa0b4fb9ddd0cba96
turupawn <ahmed.hn.43@gmail.com>
2018-06-12 21:15:18 -0600

    droped 64 bits build

--- Source/Main.hx
+++ Source/Main.hx
@@ -18,50 +18,16 @@ class Main extends Sprite {
 	private var state = "waiting for email";
 	var text_field:TextField;
 	var label:TextField;
-
-	public function emailRequest(email:String)
-	{
-		ModioWrapper.emailRequest(email, function (resonse_code:Int)
-		{
-			if (resonse_code == 200)
-			{
-				label.text = "Please enter the 5 digit code sent to your email:";
-				// A 5 digit security code will be sent to his email
-				state = "waiting for code";
-			}
-			else
-			{
-				label.text = "Error sending code";
-			}
-		});
-	}
-
-	public function emailExchange(security_code:String)
-	{
-		ModioWrapper.emailExchange(security_code, function (resonse_code:Int)
-		{
-			if (resonse_code == 200)
-			{
-				label.text = "Code exchanged! You are now logged in.";
-				text_field.visible = false;
-				// A token will be stored on the .modio/ directory.
-				// Now the user can query his mods, upload new ones and automatic downloads will be enabled on the background.
-			}
-			else
-			{
-				label.text = "Error exchanging code";
-			}
-		});
-	}
-	
 	
 	public function new () {
 		
 		super ();
 
-		//// Begin mod.io wrapper login ////
-		ModioWrapper.init(1, 7, "e91c01b8882f4affeddd56c96111977b");
+		// First let's initialize mod.io by providing the environment type, your game id and your API key
+		// You can grab your game id and API key from the mod.io web
+		ModioWrapper.init(ModioWrapper.MODIO_ENVIRONMENT_TEST, 7, "e91c01b8882f4affeddd56c96111977b");
 
+		// Now let's add a label and text field to retreive input from the user
 		label = new TextField();
 		label.text = "Please enter your email:";
 		addChild( label );
@@ -91,22 +57,54 @@ class Main extends Sprite {
 		
 		ModioWrapper.process();
 
-		if(state == "waiting for email" && user_input != "")
+		// Now let's check if the user has entered an input
+		if(user_input != "")
 		{
-			label.text = "Wating...";
-			var email = user_input;
-			user_input = "";
-			emailRequest(email);
-		}
+			if(state == "waiting for email")
+			{
+				label.text = "Wating...";
+				var email = user_input;
+				user_input = "";
 
-		if(state == "waiting for code" && user_input != "")
-		{
-			label.text = "Wating...";
-			var security_code = user_input;
-			user_input = "";
-			emailExchange(security_code);
+				// To authenticate, the user provides his email. A security code will be sent there
+				ModioWrapper.emailRequest(email, function (resonse_code:Int)
+				{
+					if (resonse_code == 200)
+					{
+						label.text = "Please enter the 5 digit code sent to your email:";
+						state = "waiting for code";
+					}
+					else
+					{
+						label.text = "Error sending code";
+					}
+				});
+			}
+			if(state == "waiting for code")
+			{
+				label.text = "Wating...";
+				var security_code = user_input;
+				user_input = "";
+				
+				// To complete the authentication, the user enters the security code sent to his email
+				ModioWrapper.emailExchange(security_code, function (resonse_code:Int)
+				{
+					if (resonse_code == 200)
+					{
+						label.text = "Code exchanged! You are now logged in.";
+						text_field.visible = false;
+						// Now an authentication token will be stores on the .modio/ folder
+						// This is handled by the SDK on the background so you don't have to worry about it
+						// Form now on, the user will be able to browse his mods, upload, subscribe, rate etc...
+						// Also, automatic uploads will be enabled so mods will be automatically installed if the user subscribes from the web, plugin, API or SDK
+						// To logout call the ModioWrapper.logout() function
+					}
+					else
+					{
+						label.text = "Error exchanging code";
+					}
+				});
+			}
 		}
 	}
-	
-	
 }
--- Source/ModioWrapper.hx
+++ Source/ModioWrapper.hx
@@ -1,14 +1,16 @@
 class ModioWrapper
 {
-  #if (linux && x64)
-    public static var init:Int->Int->String->Void = cpp.Lib.load("modioWrapperLinux","modioWrapperInit",3);
-    public static var process:Void->Void = cpp.Lib.load("modioWrapperLinux","modioWrapperProcess",0);
-    public static var isLoggedIn:Void->Bool = cpp.Lib.load("modioWrapperLinux","modioWrapperIsLoggedIn",0);
-    public static var logout:Void->Bool = cpp.Lib.load("modioWrapperLinux","modioWrapperLogout",0);
-    public static var emailRequest:String->(Int->Void)->Void = cpp.Lib.load("modioWrapperLinux","modioWrapperEmailRequest",2);
-    public static var emailExchange:String->(Int->Void)->Void = cpp.Lib.load("modioWrapperLinux","modioWrapperEmailExchange",2);
+  public static var MODIO_ENVIRONMENT_LIVE = 0;
+  public static var MODIO_ENVIRONMENT_TEST = 1;
+  #if (linux)
+    public static var init:Int->Int->String->Void = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperInit",3);
+    public static var process:Void->Void = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperProcess",0);
+    public static var isLoggedIn:Void->Bool = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperIsLoggedIn",0);
+    public static var logout:Void->Bool = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperLogout",0);
+    public static var emailRequest:String->(Int->Void)->Void = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperEmailRequest",2);
+    public static var emailExchange:String->(Int->Void)->Void = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperEmailExchange",2);
   #end
-  #if (windows && x86)
+  #if (windows)
     public static var init:Int->Int->String->Void = cpp.Lib.load("modioWrapperWindows_x86","modioWrapperInit",3);
     public static var process:Void->Void = cpp.Lib.load("modioWrapperWindows_x86","modioWrapperProcess",0);
     public static var isLoggedIn:Void->Bool = cpp.Lib.load("modioWrapperWindows_x86","modioWrapperIsLoggedIn",0);
