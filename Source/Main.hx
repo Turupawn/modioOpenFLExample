17c5e950086d49c2739ce6d77d3aa589eedb6517
turupawn <ahmed.hn.43@gmail.com>
2018-06-30 21:25:21 -0600

    greeting the player

--- Source/Main.hx
+++ Source/Main.hx
@@ -13,11 +13,47 @@ import openfl.ui.Keyboard;
 
 class Main extends Sprite {
 	
-
 	private var user_input = "";
 	private var state = "waiting for email";
+	var authentication_text:TextField;
 	var text_field:TextField;
-	var label:TextField;
+	var download_listener_text:TextField;
+	var download_progress_text:TextField;
+	var current_user_text:TextField;
+
+	public function displayAuthenticatedUser()
+	{
+		ModioWrapper.getAuthenticatedUser(function(response_code:Int, user:Dynamic)
+		{
+			if(response_code == 200)
+			{
+				current_user_text.text = "Hello " + user.username + "!";
+			}else
+			{
+				current_user_text.text = "Error retrieving the authenticated user info.";
+			}
+		});
+	}
+
+	public function displayDownloadProgress()
+	{
+		var download_queue:Array<Dynamic>;
+		download_queue = ModioWrapper.getModDownloadQueue();
+		if(download_queue.length > 0)
+		{
+			var progress = download_queue[0].current_progress / download_queue[0].total_size;
+			progress = Math.round( progress * 100 * Math.pow(10, 2) ) / Math.pow(10, 2);
+			var download_progress = "Downloading... ";
+			if(download_queue[0].current_progress > 0)
+				download_progress += progress + "%";
+			if(download_queue.length > 1)
+				download_progress += " (" + (download_queue.length - 1) + " downloads queued)";
+			download_progress_text.text = download_progress;
+		}else
+		{
+			download_progress_text.text = "";
+		}
+	}
 	
 	public function new () {
 		
@@ -28,18 +64,68 @@ class Main extends Sprite {
 		ModioWrapper.init(ModioWrapper.MODIO_ENVIRONMENT_TEST, 7, "e91c01b8882f4affeddd56c96111977b");
 
 		// Now let's add a label and text field to retreive input from the user
-		label = new TextField();
-		label.text = "Please enter your email:";
-		addChild( label );
+		authentication_text = new TextField();
+		authentication_text.text = "Please enter your email:";
+		addChild( authentication_text );
 		
 		text_field = new TextField();
-        text_field.text = "";
-		text_field.y = 40;
+		text_field.text = "";
+		text_field.y = 20;
 		text_field.height = 20;
 		text_field.width = 200;
-        text_field.type = TextFieldType.INPUT;
-        text_field.border = true;
-        addChild( text_field );
+		text_field.type = TextFieldType.INPUT;
+		text_field.border = true;
+		addChild( text_field );
+
+		download_listener_text = new TextField();
+		download_listener_text.text = "";
+		download_listener_text.y = 100;
+		download_listener_text.height = 20;
+		download_listener_text.width = 200;
+		addChild( download_listener_text );
+
+		download_progress_text = new TextField();
+		download_progress_text.text = "";
+		download_progress_text.y = 150;
+		download_progress_text.height = 20;
+		download_progress_text.width = 200;
+		addChild( download_progress_text );
+
+		current_user_text = new TextField();
+		current_user_text.text = "Loading...";
+		current_user_text.x = 350;
+		current_user_text.height = 20;
+		current_user_text.width = 200;
+		addChild( current_user_text );
+
+		if(ModioWrapper.isLoggedIn())
+		{
+			download_listener_text.text = "Listening to events...";
+			displayAuthenticatedUser();
+		}else
+		{
+			current_user_text.text = "You are not logged in";
+		}
+		
+		ModioWrapper.setDownloadListener(function(response_code:Int, mod_id:Int)
+		{
+			if(response_code == 200 || response_code == 206)
+			{
+				download_listener_text.text = "Mod installed! Mod id: " + mod_id;
+				var installed_mods:Array<Dynamic>;
+				installed_mods = ModioWrapper.getInstalledMods();
+				for(i in 0...installed_mods.length)
+				{
+					if(installed_mods[i].mod_id == mod_id)
+					{
+						download_listener_text.text += " Local path: " + installed_mods[i].path;
+					}
+				}
+			}else
+			{
+				download_listener_text.text = "Error installing mod.";
+			}
+		});
 		
 		addEventListener (Event.ENTER_FRAME, this_onEnterFrame);
 
@@ -54,15 +140,15 @@ class Main extends Sprite {
 	}
 	
 	private function this_onEnterFrame (event:Event):Void {
-		
 		ModioWrapper.process();
+		displayDownloadProgress();
 
 		// Now let's check if the user has entered an input
 		if(user_input != "")
 		{
 			if(state == "waiting for email")
 			{
-				label.text = "Wating...";
+				authentication_text.text = "Wating...";
 				var email = user_input;
 				user_input = "";
 
@@ -71,18 +157,18 @@ class Main extends Sprite {
 				{
 					if (resonse_code == 200)
 					{
-						label.text = "Please enter the 5 digit code sent to your email:";
+						authentication_text.text = "Please enter the 5 digit code sent to your email:";
 						state = "waiting for code";
 					}
 					else
 					{
-						label.text = "Error sending code";
+						authentication_text.text = "Error sending code";
 					}
 				});
 			}
 			if(state == "waiting for code")
 			{
-				label.text = "Wating...";
+				authentication_text.text = "Wating...";
 				var security_code = user_input;
 				user_input = "";
 				
@@ -91,8 +177,10 @@ class Main extends Sprite {
 				{
 					if (resonse_code == 200)
 					{
-						label.text = "Code exchanged! You are now logged in.";
+						authentication_text.text = "Code exchanged! You are now logged in.";
 						text_field.visible = false;
+						download_listener_text.text = "Listening to events...";
+						displayAuthenticatedUser();
 						// Now an authentication token will be stores on the .modio/ folder
 						// This is handled by the SDK on the background so you don't have to worry about it
 						// Form now on, the user will be able to browse his mods, upload, subscribe, rate etc...
@@ -101,7 +189,7 @@ class Main extends Sprite {
 					}
 					else
 					{
-						label.text = "Error exchanging code";
+						authentication_text.text = "Error exchanging code";
 					}
 				});
 			}
--- Source/ModioWrapper.hx
+++ Source/ModioWrapper.hx
@@ -1,21 +1,82 @@
+class ModCreator
+{
+    public var logo_path:String;
+    public var name:String;
+    public var summary:String;
+    public function new()
+    {
+        logo_path = "";
+        name = "";
+        summary = "";
+    }
+}
+
+class ModEditor
+{
+    public var logo_path:String;
+    public var name:String;
+    public var summary:String;
+    public function new()
+    {
+        logo_path = "";
+        name = "";
+        summary = "";
+    }
+}
+
+class ModfileCreator
+{
+    public var logo_path:String;
+    public var name:String;
+    public var summary:String;
+    public function new()
+    {
+        logo_path = "";
+        name = "";
+        summary = "";
+    }
+}
+
 class ModioWrapper
 {
   public static var MODIO_ENVIRONMENT_LIVE = 0;
   public static var MODIO_ENVIRONMENT_TEST = 1;
+
+  public static var MODIO_SORT_BY_ID            = 0;
+  public static var MODIO_SORT_BY_RATING        = 1;
+  public static var MODIO_SORT_BY_DATE_LIVE     = 2;
+  public static var MODIO_SORT_BY_DATE_UPDATED  = 3;
+
   #if (linux)
-    public static var init:Int->Int->String->Void = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperInit",3);
-    public static var process:Void->Void = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperProcess",0);
-    public static var isLoggedIn:Void->Bool = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperIsLoggedIn",0);
-    public static var logout:Void->Bool = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperLogout",0);
-    public static var emailRequest:String->(Int->Void)->Void = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperEmailRequest",2);
-    public static var emailExchange:String->(Int->Void)->Void = cpp.Lib.load("modioWrapperLinux_x64","modioWrapperEmailExchange",2);
+    static var ndll_name:String = "modioWrapperLinux_x64";
   #end
   #if (windows)
-    public static var init:Int->Int->String->Void = cpp.Lib.load("modioWrapperWindows_x86","modioWrapperInit",3);
-    public static var process:Void->Void = cpp.Lib.load("modioWrapperWindows_x86","modioWrapperProcess",0);
-    public static var isLoggedIn:Void->Bool = cpp.Lib.load("modioWrapperWindows_x86","modioWrapperIsLoggedIn",0);
-    public static var logout:Void->Bool = cpp.Lib.load("modioWrapperWindows_x86","modioWrapperLogout",0);
-    public static var emailRequest:String->(Int->Void)->Void = cpp.Lib.load("modioWrapperWindows_x86","modioWrapperEmailRequest",2);
-    public static var emailExchange:String->(Int->Void)->Void = cpp.Lib.load("modioWrapperWindows_x86","modioWrapperEmailExchange",2);
+    static var ndll_name:String = "modioWrapperWindows_x86";
   #end
+
+  public static var init:Int->Int->String->Void = cpp.Lib.load(ndll_name,"modioWrapperInit",3);
+  public static var process:Void->Void = cpp.Lib.load(ndll_name,"modioWrapperProcess",0);
+  
+  public static var emailRequest:String->(Int->Void)->Void = cpp.Lib.load(ndll_name,"modioWrapperEmailRequest",2);
+  public static var emailExchange:String->(Int->Void)->Void = cpp.Lib.load(ndll_name,"modioWrapperEmailExchange",2);
+  public static var isLoggedIn:Void->Bool = cpp.Lib.load(ndll_name,"modioWrapperIsLoggedIn",0);
+  public static var logout:Void->Bool = cpp.Lib.load(ndll_name,"modioWrapperLogout",0);
+  public static var getMods:Int->Int->Int->(Array<Dynamic>->Int->Void)->Int = cpp.Lib.load(ndll_name,"modioWrapperGetMods",4);
+  public static var subscribeToMod:Int->(Int->Void)->Void = cpp.Lib.load(ndll_name,"modioWrapperSubscribeToMod",2);
+  public static var unsubscribeFromMod:Int->(Int->Void)->Void = cpp.Lib.load(ndll_name,"modioWrapperUnsubscribeFromMod",2);
+  public static var installMod:Int->Void = cpp.Lib.load(ndll_name,"modioWrapperInstallMod",1);
+  public static var uninstallMod:Int->Void = cpp.Lib.load(ndll_name,"modioWrapperUninstallMod",1);
+  public static var pauseDownloads:Void->Void = cpp.Lib.load(ndll_name,"modioWrapperPauseDownloads",0);
+  public static var resumeDownloads:Void->Void = cpp.Lib.load(ndll_name,"modioWrapperResumeDownloads",0);
+  public static var prioritizeDownload:Int->Void = cpp.Lib.load(ndll_name,"modioWrapperPrioritizeDownload",1);
+  public static var setDownloadListener:(Int->Int->Void)->Void = cpp.Lib.load(ndll_name,"modioWrapperSetDownloadListener",1);
+  public static var getModDownloadQueue:Void->Array<Dynamic> = cpp.Lib.load(ndll_name,"modioWrapperGetModDownloadQueue",0);
+  public static var setUploadListener:(Int->Int->Void)->Void = cpp.Lib.load(ndll_name,"modioWrapperSetUploadListener",1);
+  public static var getModfileUploadQueue:Void->Array<Dynamic> = cpp.Lib.load(ndll_name,"modioWrapperGetModfileUploadQueue",0);
+  public static var getInstalledMods:Void->Array<Dynamic> = cpp.Lib.load(ndll_name,"modioWrapperGetInstalledMods",0);
+  public static var getModState:Int->Int = cpp.Lib.load(ndll_name,"modioWrapperGetModState",1);
+  public static var addMod:ModCreator->(Int->Void)->Int = cpp.Lib.load(ndll_name,"modioWrapperAddMod", 2);
+  public static var editMod:Int->ModEditor->(Int->Void)->Int = cpp.Lib.load(ndll_name,"modioWrapperEditMod", 3);
+  public static var addModfile:Int->ModfileCreator->(Int->Void)->Int = cpp.Lib.load(ndll_name,"modioWrapperAddModfile", 2);
+  public static var getAuthenticatedUser:(Int->Dynamic->Void)->Int = cpp.Lib.load(ndll_name,"modioWrapperGetAuthenticatedUser", 1);
 }
